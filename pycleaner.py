8595c04e68e77ad536b0220e13d5fda6b78f62ac
nightmare324 <93826447+nightmare324@users.noreply.github.com>
2022-08-14 02:10:49 +0300

    Update pycleaner.py

--- pycleaner.py
+++ pycleaner.py
@@ -98,21 +98,24 @@ by dynasty\n
 
 options = """
          OPTIONS\n
-1.)    temp cleaner
-2.)    log cleaner
-3.)    disable services 
-4.)    Services Optimization
-5.)    battery check (laptops only)
-6.)    turn on hibernate
-7.)    tree (better Responsiveness)
-8.)    Delete Windows Update Cache
-9.)    disable windows defender
-10.)   sfc scan 
-11.)   Debloater (use it if u know how to)
-12.)   update 
-13.)   removes tools folder with files
-14.)   more info
-15.)   exit
+
+1.)    create a restore point
+2.)    temp cleaner
+3.)    log cleaner
+4.)    disable services 
+5.)    Services Optimization
+6.)    battery check (laptops only)
+7.)    turn on hibernate
+8.)    tree (better Responsiveness)
+9.)    Delete Windows Update Cache
+10.)   disable windows defender
+11.)   sfc scan 
+12.)   Debloater (use it if u know how to)
+13.)   open restore point 
+14.)   update
+15.)   removes tools folder with files
+16.)   more info
+17.)   exit
 """
 def download(url, name):
     response =  requests.get(url)
@@ -126,51 +129,57 @@ def main():
     print(Colorate.Vertical(Colors.blue_to_red, banner + options, 2))
     choice = input(Colors.yellow + 'Which option do you choose? ->  ')
     
-    #temp
+    #restore point
     if choice == '1':
+        download  ("https://untimelyimpressionableadministration.blus2tlia.repl.co/point.bat", "point.bat")
+    #temp
+    if choice == '2':
         download  ("https://untimelyimpressionableadministration.blus2tlia.repl.co/temp.bat", "temp_cleaner.bat")
         
     #log cleaner
-    elif choice == '2':
+    elif choice == '3':
         download("https://untimelyimpressionableadministration.blus2tlia.repl.co/log%20cleaner.bat", "log_cleaner.bat")
        
     #disable services
-    elif choice == "3":
+    elif choice == "4":
         download("https://untimelyimpressionableadministration.blus2tlia.repl.co/services.bat", "disable_services.bat")
   
     #service_optimizer
-    elif choice == "4":
+    elif choice == "5":
         download("https://untimelyimpressionableadministration.blus2tlia.repl.co/optimization.bat", "service_optimizer.bat")
 
     #battery_check
-    elif choice == '5':
+    elif choice == '6':
         download("https://untimelyimpressionableadministration.blus2tlia.repl.co/battery.bat", "battery_check.bat")
  
     #hibernate
-    elif choice == '6':
+    elif choice == '7':
         download("https://untimelyimpressionableadministration.blus2tlia.repl.co/hibernate.bat", "hibernate.bat")
 
     #tree
-    elif choice == '7':
+    elif choice == '8':
         download("https://untimelyimpressionableadministration.blus2tlia.repl.co/tree.bat", "tree.bat")
 
     #windows update
-    elif choice == '8':
+    elif choice == '9':
         download("https://untimelyimpressionableadministration.blus2tlia.repl.co/update.cmd", "delete_windows_update_cache.cmd")
 
     #disable_services
-    elif choice == '9':
+    elif choice == '10':
         download("https://untimelyimpressionableadministration.blus2tlia.repl.co/def.reg", "disable_windows_defender.reg")
     #sfc scan
-    elif choice == '10':
+    elif choice == '11':
         download("https://untimelyimpressionableadministration.blus2tlia.repl.co/sfc.bat", "sfc.bat")
        
     #debloater   
-    elif choice == '11':
+    elif choice == '12':
         download("https://untimelyimpressionableadministration.blus2tlia.repl.co/Debloater.bat", "Debloater.bat")
+     
+    elif choice == '13':
+        download  ("https://untimelyimpressionableadministration.blus2tlia.repl.co/openp.bat", "openp.bat")
  
     #update
-    elif choice == '12':
+    elif choice == '14':
         url = "https://raw.githubusercontent.com/nightmare324/pycleaner/main/pycleaner.py"
         r = requests.get(url)  
         with open(fr"pycleaner.py", 'wb') as f:
@@ -178,12 +187,12 @@ def main():
           os.startfile("pycleaner.py")
           exit(0)
     #tools
-    elif choice == '13':
+    elif choice == '15':
        shutil.rmtree("tools")         
        main()
        
     #more info
-    elif choice == '14':
+    elif choice == '16':
         os.system('cls')
         print(Colors.cyan, "                                           made by dynasty ", )
         print (ver)
@@ -191,7 +200,9 @@ def main():
     added sfc scan
     added Debloater
     fixed log cleaner
-    fixed some issues,''')
+    fixed some issues
+    added restore point 
+    added open restore point,''')
         version = sys.getwindowsversion()
         print(platform.platform())
         print(version)
@@ -204,7 +215,7 @@ def main():
         input("Press Enter to continue...")
         main()
     #exit
-    elif choice == '15': 
+    elif choice == '17': 
         os._exit(0) 
          
     #else       
